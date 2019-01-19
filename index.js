c1bfc238b43f53cff04b32546dd24c8c78c6e829
Rajiv Shah <rajivshah1@icloud.com>
2019-01-19 00:13:32 -0500

    Linting

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 'use strict';
 
-import {NativeModules, Platform} from 'react-native';
-const {RNDetectNavbarAndroid} = NativeModules;
+import { NativeModules, Platform } from 'react-native';
+const { RNDetectNavbarAndroid } = NativeModules;
 
 function unSupportedError() {
   throw new Error('[react-native-detect-navbar-android] does not support iOS');
 }
 
-const DetectNavbar = Platform.OS === 'android' ? {
-  hasSoftKeys() {
-    return RNDetectNavbarAndroid.hasSoftKeys();
-  }
-} : {
-  hasSoftKeys: unSupportedError
-};
+const DetectNavbar =
+  Platform.OS === 'android'
+    ? {
+        hasSoftKeys() {
+          return RNDetectNavbarAndroid.hasSoftKeys();
+        },
+      }
+    : {
+        hasSoftKeys: unSupportedError,
+      };
 
-export {DetectNavbar};
+export { DetectNavbar };
 export default DetectNavbar;
