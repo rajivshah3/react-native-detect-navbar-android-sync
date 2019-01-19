7c811924972be2400eef203790486e00f64e13e3
Rajiv Shah <rajivshah1@icloud.com>
2019-01-19 00:25:01 -0500

    Refactor

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-'use strict';
-
 import { NativeModules, Platform } from 'react-native';
+
 const { RNDetectNavbarAndroid } = NativeModules;
 
 function unSupportedError() {
-  throw new Error('[react-native-detect-navbar-android] does not support iOS');
+  throw new Error('[react-native-detect-navbar-android-sync] does not support iOS');
 }
 
-const DetectNavbar =
-  Platform.OS === 'android'
-    ? {
-        hasSoftKeys() {
-          return RNDetectNavbarAndroid.hasSoftKeys();
-        },
-      }
-    : {
-        hasSoftKeys: unSupportedError,
-      };
+export const DetectNavbar = () => {
+  if (Platform.OS === 'android') {
+    return {
+      hasSoftKeys() {
+        return RNDetectNavbarAndroid.hasSoftKeys();
+      },
+    };
+  }
+  return { hasSoftKeys: unSupportedError };
+};
 
-export { DetectNavbar };
 export default DetectNavbar;
